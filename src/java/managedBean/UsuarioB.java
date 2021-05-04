82cb5a429c5dada361289bd983da1cf5e88ab33f
Lucas <Lucas@192.168.1.2>
2021-05-04 16:12:37 -0300

    Produto e Usuario DAO criado

--- src/java/managedBean/UsuarioB.java
+++ src/java/managedBean/UsuarioB.java
@@ -7,6 +7,7 @@ package managedBean;
 
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 
 /**
  *
@@ -16,10 +17,88 @@ import javax.enterprise.context.RequestScoped;
 @RequestScoped
 public class UsuarioB {
 
-    /**
-     * Creates a new instance of UsuarioB
-     */
+    private String nome;
+    private String email;
+    private String senha;
+    private String cpf;
+    private String cep;
+    
+    @Inject
+    
     public UsuarioB() {
     }
     
+    public void salvarUsuario(){
+        
+    }
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the cep
+     */
+    public String getCep() {
+        return cep;
+    }
+
+    /**
+     * @param cep the cep to set
+     */
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
 }
